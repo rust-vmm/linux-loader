642627a252d99732a5aaa66b4bf2b8ca40cc464e
Alexandra Iordache <aghecen@amazon.com>
2020-07-14 16:34:58 +0300

    benchmark: add initial benchmark tests
    These tests use the criterion plugin to collect
    performance data for a given pub fn (selected 2 PVH
    related functions for their relative complexity, as
    well as a simple test for ARM) and (locally) compare
    it with future runs.
    
    This commit is part of a larger story spanning the
    whole rust-vmm, kickstarting a centralized effort to
    add benchmarks to crates used in production VMMs.
    
    Signed-off-by: Alexandra Iordache <aghecen@amazon.com>

---
+++ benches/main.rs
@@ -0,0 +1,31 @@
+// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE-BSD-3-Clause file.
+//
+// SPDX-License-Identifier: Apache-2.0 AND BSD-3-Clause
+extern crate criterion;
+extern crate linux_loader;
+extern crate vm_memory;
+
+use criterion::{criterion_group, criterion_main, Criterion};
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+mod x86_64;
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+use x86_64::*;
+
+#[cfg(target_arch = "aarch64")]
+mod aarch64;
+#[cfg(target_arch = "aarch64")]
+use aarch64::*;
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(500);
+    targets = criterion_benchmark
+}
+
+criterion_main! {
+    benches
+}
