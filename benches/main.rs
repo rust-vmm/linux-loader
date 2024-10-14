27e61be1f5c13df5f46fba85eb61706e73722673
Ruoqing He <heruoqing@iscas.ac.cn>
2024-10-14 20:11:41 +0800

    aarch64: Remove redundant bench
    The `criterion_benchmark_nop` function does literally nothing, there is
    no need to run these benchs.
    
    Re-organize the code to run `benches_bzimage` on x86 and x86_64 machines
    with `bzimage` feature enabled.
    
    Co-authored-by: Patrick Roy <roypat@amazon.co.uk>
    Signed-off-by: Ruoqing He <heruoqing@iscas.ac.cn>

--- benches/main.rs
+++ benches/main.rs
@@ -21,37 +21,12 @@ mod aarch64;
 #[cfg(target_arch = "aarch64")]
 use aarch64::*;
 
-pub fn criterion_benchmark_nop(_: &mut Criterion) {}
-
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(500);
     targets = criterion_benchmark
 }
 
-#[cfg(all(any(target_arch = "x86", target_arch = "x86_64"), feature = "bzimage"))]
-// Explicit (arch, feature) tuple required as clippy complains about
-// `criterion_benchmark_bzimage` missing on aarch64.
-criterion_group! {
-    name = benches_bzimage;
-    // Only ~125 runs fit in 5 seconds. Either extend the duration, or reduce
-    // the number of iterations.
-    config = Criterion::default().sample_size(100);
-    targets = criterion_benchmark_bzimage
-}
-
-// NOP because the `criterion_main!` macro doesn't support cfg(feature)
-// macro expansions.
-#[cfg(any(target_arch = "aarch64", not(feature = "bzimage")))]
-criterion_group! {
-    name = benches_bzimage;
-    // Sample size must be >= 10.
-    // https://github.com/bheisler/criterion.rs/blob/0.3.0/src/lib.rs#L757
-    config = Criterion::default().sample_size(10);
-    targets = criterion_benchmark_nop
-}
-
 criterion_main! {
-    benches,
-    benches_bzimage
+    benches
 }
