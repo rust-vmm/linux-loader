df0098aa2d739e79f6c9a87e25a80a4c492bffe2
Ruoqing He <heruoqing@iscas.ac.cn>
2024-10-20 19:55:18 +0800

    configurator: Re-organize module layout
    Code in `fdt.rs` do not contain architecture specific code as of
    `aarch64`, it's only used by `aarch64`, move it one level up to allow
    reusing on riscv64 platform.
    
    Signed-off-by: Ruoqing He <heruoqing@iscas.ac.cn>

--- src/configurator/mod.rs
+++ src/configurator/mod.rs
@@ -28,9 +28,8 @@ mod x86_64;
 pub use x86_64::*;
 
 #[cfg(target_arch = "aarch64")]
-mod aarch64;
-#[cfg(target_arch = "aarch64")]
-pub use aarch64::*;
+pub mod fdt;
+
 use std::cmp::max;
 use std::mem::size_of;
 
