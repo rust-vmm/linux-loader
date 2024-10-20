1ca7e3d1aa99d43cab5d81cd5ee576b2de5b535b
Ruoqing He <heruoqing@iscas.ac.cn>
2024-10-20 19:55:18 +0800

    configurator: Re-organize module layout
    Code in `fdt.rs` do not contain architecture specific code as of
    `aarch64`, it's only used by `aarch64`, move it one level up to allow
    reusing on riscv64 platform.
    
    Signed-off-by: Ruoqing He <heruoqing@iscas.ac.cn>

--- src/configurator/aarch64/mod.rs
+++
@@ -1,7 +0,0 @@
-// Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-//
-// SPDX-License-Identifier: Apache-2.0 AND BSD-3-Clause
-
-//! Traits and structs for configuring and loading boot parameters on `aarch64`.
-
-pub mod fdt;
